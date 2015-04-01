59e193ff5b5ac587d4f97f868d0b87e908eb84c9
Viktor Gamov <gamussa@gmail.com>
2015-04-01 10:08:41 -0400

    updated underscore example with jvm-npm + npm and node ignorings
    This reverts commit 023b2780dbacac07bc996f564993797fd5ea2a32.

--- src/main/resources/javascript/play_with_underscore.js
+++ src/main/resources/javascript/play_with_underscore.js
@@ -1,31 +1,12 @@
-#!/usr/bin/env jjs -scripting
+
+
+
 
 //load('http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min.js');
-//load('./lib/underscore-min.js');
-load('./lib/jvm-npm.js');
-var _ = require('underscore');
+load('./lib/underscore-min.js');
 
 var odds = _.filter([1, 2, 3, 4, 5, 6], function (num) {
     return num % 2 == 1;
 });
 
 print(odds);  // 1, 3, 5
-
-
-var each = _.each([1, 2, 3], function (it) {
-    return it;
-});
-
-print(each);
-
-var map = _.map([1, 2, 3], function (num) {
-    return num * 3;
-});
-
-print(map);
-
-var reduce = _.reduce([1, 2, 3], function (memo, num) {
-    return memo + num;
-}, 0);
-
-print(reduce);
